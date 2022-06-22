bd42edb786cbb66499d887963de52b46e6cb2acb
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-06-22 15:02:54 +0300

    feat: search car page component

---
+++ src/components/page/SearchCarPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchForm from '../SearchForm'
+
+const SearchCarPage = () => {
+  return (
+    <div>
+        <h1>Search Car</h1>
+        <SearchForm />
+    </div>
+  )
+}
+
+export default SearchCarPage
